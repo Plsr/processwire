3561f316fe2b9e67f264cca605167c717824419f
plsr <christianpoplawski@gmail.com>
2014-10-25 17:29:04 +0200

    Initial commit

---
+++ site/config.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * ProcessWire Configuration File
+ *
+ * Site-specific configuration for ProcessWire
+ * 
+ * Please see the file /wire/config.php which contains all configuration options you may
+ * specify here. Simply copy any of the configuration options from that file and paste
+ * them into this file in order to modify them. 
+ *
+ * ProcessWire 2.x 
+ * Copyright (C) 2014 by Ryan Cramer 
+ * Licensed under GNU/GPL v2, see LICENSE.TXT
+ * 
+ * http://processwire.com
+ *
+ */
+
+if(!defined("PROCESSWIRE")) die();
+
+/*** SITE CONFIG *************************************************************************/
+
+/**
+ * Enable debug mode?
+ *
+ * Debug mode causes additional info to appear for use during dev and debugging.
+ * This is almost always recommended for sites in development. However, you should
+ * always have this disabled for live/production sites.
+ *
+ * @var bool
+ *
+ */
+$config->debug = false;
+
+/**
+ * Prepend template file
+ *
+ * PHP file in /site/templates/ that will be loaded before each page's template file.
+ * Example: _init.php
+ *
+ * @var string
+ *
+ */
+$config->prependTemplateFile = '_init.php';
+
+/**
+ * Append template file
+ *
+ * PHP file in /site/templates/ that will be loaded after each page's template file.
+ * Example: _main.php
+ *
+ * @var string
+ *
+ */
+$config->appendTemplateFile = '_main.php';
+
+
+
+
+/*** INSTALLER CONFIG ********************************************************************/
+
+/**
+ * Installer: Database Configuration
+ * 
+ */
+$config->dbHost = 'localhost';
+$config->dbName = 'processwire';
+$config->dbUser = 'root';
+$config->dbPass = 'root';
+$config->dbPort = '8888';
+
+/**
+ * Installer: User Authentication Salt 
+ * 
+ * Must be retained if you migrate your site from one server to another
+ * 
+ */
+$config->userAuthSalt = 'e6f5336e50de93d303d3c826eabd1051'; 
+
+/**
+ * Installer: File Permission Configuration
+ * 
+ */
+$config->chmodDir = '0755'; // permission for directories created by ProcessWire
+$config->chmodFile = '0644'; // permission for files created by ProcessWire 
+
+/**
+ * Installer: Time zone setting
+ * 
+ */
+$config->timezone = 'Europe/Berlin';
+
+
+/**
+ * Installer: HTTP Hosts Whitelist
+ * 
+ */
+$config->httpHosts = array('localhost:8888', 'localhost');
+
