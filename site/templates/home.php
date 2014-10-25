524304be19f3317b1c0933997e1f3f503e9f1d74
plsr <christianpoplawski@gmail.com>
2014-10-26 00:41:48 +0200

    Added alternative syntax for control structures to loop

--- site/templates/home.php
+++ site/templates/home.php
@@ -11,14 +11,14 @@ include('./includes/header.inc');
 <?php
 	//	Main Loop of the page.
 	//	Gets all posts from the post-wrapper page and displays their contents
-	foreach($pages->get('/post-wrapper/')->children as $item) {
+	foreach($pages->get('/post-wrapper/')->children as $item) :
 		//	Only display posts with the 'post' template active
-		if($item->template->id == $templates->get('post')->id){
-			echo $item->title;
-			echo $item->post_content;
-		}
-	}
-?>
+		if($item->template->id == $templates->get('post')->id) : ?>
+			<h1><?php echo $item->title; ?></h1>
+			<p><?php echo $item->post_content; ?></p>
+		
+		<?php endif;?>
+<?php endforeach;?>
 
 
 <?php 
